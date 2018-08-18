bd3ce2324215df075e0feb00268e5306f9f38078
Mai Chee Vang <maicheevang@Mais-MacBook-Pro.local>
2018-08-17 22:31:55 -0500

    Config set up functional.

---
+++ server/public/scripts/client.js
@@ -0,0 +1,21 @@
+const myApp = angular.module('myApp', ['ngRoute']);
+console.log('js loaded');
+
+myApp.config(function ($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeController as hc'
+    })
+    .when('/sale', {
+        templateUrl: '/views/sale.html',
+        controller: 'SaleController as sc'
+    })
+    .when('/rent', {
+        templateUrl: 'views/rent.html',
+        controller: 'RentController as rc'
+    })
+    .otherwise({
+        templateUrl: 'views/404.html'
+    });
+}); //end of myApp.config
+
